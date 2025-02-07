9b62e4f48b44613340b9970520bd1b84fc80da8e
Daniel Schreiber <daniel-schreiber@gmx.de>
2025-02-07 20:28:12 +0100

    feat: configurable via args + help

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
 mod get_state;
 mod write_csv;
 
-use std::env;
+use crate::get_state::State;
 use chrono::Local;
+use std::env;
+use std::path::Path;
 use std::thread::sleep;
 use std::time::Duration;
 
-const FREQUENCY: f64 = 2.0; // seconds
+const DEFAULT_FREQUENCY: f64 = 2.0; // seconds
 const ARG_STDOUT: &str = "--stdout";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut prev_state = get_state::get_state();
+    if args.iter().any(|arg| arg == "--help") {
+        println!("Usage: work_log [OPTION]...");
+        println!("Log the active window and cursor position to a CSV file.");
+        println!();
+        println!("Options:");
+        println!("  --frequency=SECONDS  set the frequency of logging (default: 2.0)");
+        println!("  --file=FILE          set the file to write to (default: ~/work_log.csv)");
+        println!("  --stdout             print to stdout instead of a file");
+        return;
+    }
+    let freq = args
+        .iter()
+        .find(|arg| arg.starts_with("--frequency="))
+        .map(|arg| arg.split('=').last().unwrap())
+        .map(|arg| arg.parse::<f64>().unwrap_or(DEFAULT_FREQUENCY))
+        .unwrap_or(DEFAULT_FREQUENCY);
+    let user_home = env::var("HOME").unwrap_or(env::var("USERPROFILE").unwrap_or(".".to_string()));
+    let default_file = Path::new(&user_home).join(Path::new("work_log.csv"));
+    let file = args
+        .iter()
+        .find(|arg| arg.starts_with("--file="))
+        .map(|arg| {
+            Path::new(
+                arg.split('=')
+                    .last()
+                    .expect(&format!("Invalid argument: {}", arg)),
+            )
+        })
+        .unwrap_or(&default_file);
+    println!("Logging to {}", file.display());
+
+    let mut prev_state: State = get_state::get_state();
     loop {
         let current_time = Local::now();
         let state = get_state::get_state();
@@ -19,12 +52,11 @@ fn main() {
             if args.iter().any(|arg| arg == ARG_STDOUT) {
                 println!("{}", state.to_csv(&current_time));
             } else {
-                let csv_filename = format!("logs/activity_log_{}.csv", current_time.format("%Y_%m_%d"));
-                write_csv::add_to_csv(&csv_filename, &current_time, &state);
+                write_csv::add_to_csv(file, &current_time, &state);
             }
             prev_state = state;
         }
 
-        sleep(Duration::from_secs_f64(FREQUENCY));
+        sleep(Duration::from_secs_f64(freq));
     }
 }
--- src/write_csv.rs
+++ src/write_csv.rs
@@ -1,8 +1,8 @@
+use crate::get_state;
 use chrono::Local;
 use std::fs::{create_dir_all, OpenOptions};
-use std::path::Path;
 use std::io::Write;
-use crate::get_state;
+use std::path::Path;
 
 
 /// Appends a log line to the CSV file.
@@ -10,12 +10,12 @@ use crate::get_state;
 /// The log line has the format:
 /// `YYYY-MM-DD HH:MM:SS.mmm;window_title_without_semicolon;x,y`
 pub(crate) fn add_to_csv(
-    csv_filename: &str,
+    csv_filename: &Path,
     current_time: &chrono::DateTime<Local>,
     state: &get_state::State,
 ) {
     // Ensure that the parent directory exists
-    if let Some(parent) = Path::new(csv_filename).parent() {
+    if let Some(parent) = csv_filename.parent() {
         if !parent.exists() {
             println!("creating {}", parent.display());
             if let Err(e) = create_dir_all(parent) {
@@ -32,11 +32,11 @@ pub(crate) fn add_to_csv(
     match file_result {
         Ok(mut file) => {
             if let Err(e) = writeln!(file, "{}", state.to_csv(&current_time)) {
-                eprintln!("Error writing to file {}: {}", csv_filename, e);
+                eprintln!("Error writing to file {}: {}", csv_filename.display(), e);
             }
         }
         Err(e) => {
-            eprintln!("Error opening file {}: {}", csv_filename, e);
+            eprintln!("Error opening file {}: {}", csv_filename.display(), e);
         }
     }
 }
